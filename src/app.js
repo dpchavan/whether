2203285bbce3f53a1e1e944264d574e012b08fe4
root <root@PRASAD-1994.localdomain>
2019-12-25 21:37:49 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const app = express();
+
+var publicDirPath = path.join(__dirname, '../templates/views');
+var partialDirPath = path.join(__dirname, '../templates/partial');
+//set hanldebar engine and views location
+app.set('view engine', 'hbs');
+app.set('views',publicDirPath);
+hbs.registerPartials(partialDirPath);
+
+//set up static directory to serve
+app.use(express.static(publicDirPath));
+
+app.get('/about', (req, res) => {
+    res.render('index',{
+        title : "about"
+    })
+});
+app.get('/help', (req, res) => {
+    res.render('index',{
+        title : "help"
+    })
+});
+app.get('/json', (req, res) => {
+    res.json({
+        name : "Prasad",
+        role : "Developer"
+    });
+});
+
+app.get('*', (req, res) => {
+    res.sendStatus(404);
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+})
